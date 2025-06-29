669f3fdf25aa8d613b05dedf8e791e18442d1885
Hamna Qamar <hamnaqamar2004@gmail.com>
2025-06-30 04:46:19 +0500

    updated app.py

--- app.py
+++ app.py
@@ -7,17 +7,17 @@ from core.model import TrainedModel
 st.set_page_config(page_title="NeuroVision Digit Classifier", layout="centered")
 st.title("Draw a Digit - NeuroVision Classifier")
 
-# --- Sidebar settings ---
+
 st.sidebar.header("Canvas Settings")
 stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 15)
 invert_input = st.sidebar.checkbox("Invert Input (if prediction is bad)", value=False)
 
-# --- Drawing canvas ---
+
 st.write("Draw a digit (0-9) below:")
 canvas_result = st_canvas(
-    fill_color="#000000",      # Ink color when filling (black strokes)
+    fill_color="#000000",      
     stroke_width=stroke_width,
-    stroke_color="#FFFFFF",    # White pen
+    stroke_color="#FFFFFF",    
     background_color="#000000",
     width=280,
     height=280,
@@ -25,25 +25,21 @@ canvas_result = st_canvas(
     key="canvas",
 )
 
-# --- Prediction ---
+
 if canvas_result.image_data is not None:
     image = canvas_result.image_data
 
-    # Convert to PIL grayscale image
     image_pil = Image.fromarray((image[:, :, 0]).astype(np.uint8))
-    image_pil = image_pil.resize((28, 28)).convert('L')  # MNIST style
+    image_pil = image_pil.resize((28, 28)).convert('L')  
 
     if invert_input:
-        image_pil = ImageOps.invert(image_pil)  # Optional inversion toggle
+        image_pil = ImageOps.invert(image_pil)  
 
-    # Normalize
     image_array = np.array(image_pil).astype(np.float32) / 255.0
     image_flattened = image_array.flatten()
 
-    # Display processed image
     st.image(image_pil, width=100, caption="Processed Input (28×28)")
 
-    # Model inference
     model = TrainedModel()
     prediction, confidences = model.predict(image_flattened, return_confidence=True)
 
--- core/model.py
+++ core/model.py
@@ -3,21 +3,21 @@ from core.layers import Layer_Dense, Activation_ReLU, Activation_Softmax
 
 class TrainedModel:
     def __init__(self):
-        # Initialize layers
+
         self.dense1 = Layer_Dense(784, 128)
         self.activation1 = Activation_ReLU()
         self.dense2 = Layer_Dense(128, 10)
         self.softmax = Activation_Softmax()
 
-        # Load saved weights and biases
+
         self.dense1.weights = np.load("weights/weights_dense1.npy")
         self.dense1.biases = np.load("weights/biases_dense1.npy")
         self.dense2.weights = np.load("weights/weights_dense2.npy")
         self.dense2.biases = np.load("weights/biases_dense2.npy")
 
     def predict(self, x, return_confidence=False):
-        # Forward pass
-        self.dense1.forward(x[np.newaxis, :])  # Add batch dimension
+
+        self.dense1.forward(x[np.newaxis, :]) 
         self.activation1.forward(self.dense1.output)
         self.dense2.forward(self.activation1.output)
         self.softmax.forward(self.dense2.output)
